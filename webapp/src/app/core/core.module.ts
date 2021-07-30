a4c18f4d5b43399a513b96e70a86ec5372ab34b8
Douglas Canevarollo <dcanevarollo@ibm.com>
2021-07-30 16:20:37 -0300

    Dashboard: App navigation

--- webapp/src/app/core/core.module.ts
+++ webapp/src/app/core/core.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    LoginComponent
+    LoginComponent,
+    NavigationComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  exports: [
+    NavigationComponent
   ]
 })
 export class CoreModule { }
