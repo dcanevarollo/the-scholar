d14c256a8d26a71edabaf7b1d6254a0cf1536cca
Douglas Canevarollo <dcanevarollo@ibm.com>
2021-07-30 15:18:51 -0300

    Auth: SnackBar for errors

--- webapp/src/app/shared/shared.module.ts
+++ webapp/src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { ContainerDirective } from './container.directive';
 
@@ -14,7 +15,8 @@ import { ContainerDirective } from './container.directive';
     ContainerDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSnackBarModule
   ],
   exports: [
     ContainerDirective,
