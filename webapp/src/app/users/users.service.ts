87ae726bb9bb9610341068ad1e46f299ae57fd61
Douglas Canevarollo <dcanevarollo@ibm.com>
2021-08-01 23:34:32 -0300

    Profile: Change password

--- webapp/src/app/users/users.service.ts
+++ webapp/src/app/users/users.service.ts
@@ -6,15 +6,28 @@ import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { User } from './shared/user.model';
 
+interface NewPassword {
+  currentPassword: string;
+  newPassword: string;
+}
+
 @Injectable()
 export class UsersService {
+
+  private readonly RETRIALS_COUNT = 1;
 
   constructor(private httpClient: HttpClient) { }
 
   update(data: User, id: string): Observable<User> {
     return this.httpClient
       .put<User>(`${environment.apiUrl}/users/${id}`, data)
-      .pipe(take(1));
+      .pipe(take(this.RETRIALS_COUNT));
+  }
+
+  changePassword(data: NewPassword, id: string): Observable<{}> {
+    return this.httpClient
+      .put(`${environment.apiUrl}/new-password/${id}`, data)
+      .pipe(take(this.RETRIALS_COUNT));
   }
 
 }
