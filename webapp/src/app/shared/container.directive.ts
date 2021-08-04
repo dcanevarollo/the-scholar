321b3a5b6eefc1c59619db31008127b0aefce9c9
Douglas Canevarollo <dcanevarollo@ibm.com>
2021-08-03 21:54:35 -0300

    Courses: Refactor styles

--- webapp/src/app/shared/container.directive.ts
+++ webapp/src/app/shared/container.directive.ts
@@ -6,15 +6,16 @@ import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
 export class ContainerDirective implements AfterViewInit {
 
   @Input() maxWidth = '768px';
-  @Input() disablePadding = false;
+  @Input() noPadding?: string | boolean = false;
 
   constructor(private elementRef: ElementRef) { }
 
   ngAfterViewInit(): void {
     this.elementRef.nativeElement.style.maxWidth = this.maxWidth;
+    this.elementRef.nativeElement.style.height = '100%';
     this.elementRef.nativeElement.style.margin = '0 auto';
 
-    if (!this.disablePadding)
+    if (this.noPadding !== '')
       this.elementRef.nativeElement.style.padding = '40px';
   }
 
