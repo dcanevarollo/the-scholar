a57dff2ac2e3689a8796826b75ad072810150228
Douglas Canevarollo <dcanevarollo@ibm.com>
2021-08-30 12:07:32 -0300

    Files: Download files

--- webapp/src/app/archives/archives.service.ts
+++ webapp/src/app/archives/archives.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { CrudService } from '../shared/crud.service';
@@ -32,6 +32,40 @@ export class ArchivesService extends CrudService<Archive, string> {
         observe: 'events',
         reportProgress: true
       });
+  }
+
+  download(archive: Archive): Subscription {
+    return this.httpClient
+      .get<Blob>(`${environment.apiUrl}/download/${archive.id}`, {
+        responseType: 'blob' as 'json'
+      })
+      .subscribe(response => this.handleDownload(response, archive.name));
+  }
+
+  private handleDownload(file: Blob, fileName: string): void {
+    // IE
+    if (!!window.navigator?.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(file);
+
+      return;
+    }
+
+    const blob = window.URL.createObjectURL(file);
+    const link = document.createElement('a');
+
+    link.href = blob;
+    link.download = fileName;
+
+    link.dispatchEvent(new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    }));
+
+    setTimeout(() => {
+      window.URL.revokeObjectURL(blob);
+      link.remove();
+    }, 100);
   }
 
 }
